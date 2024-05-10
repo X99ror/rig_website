0e0a7ea8cfb20843158394ea037379fc4ab310c7
X99ror <xgeror0062@gmail.com>
2024-05-10 17:11:53 +0530

    s

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styles from './navbar.module.css';
+
+function Navbar() {
+  return (
+    <div className={styles.navbar}>
+      <div className={styles.logo}></div>
+      <div className={styles.links}>
+        <div className={styles.home}>home</div>
+        <div className={styles.about}>about</div>
+        <div className={styles.projects}>projects</div>
+        <div className={styles.achievements}>achievements</div>
+        <div className={styles.events}>events</div>
+        <div className={styles.team}>team</div>
+        <div className={styles.contacts}>contacts</div>
+      </div>
+      <div className={styles.rig}>rig</div>
+      <div className={styles.toggle}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      
+    </div>
+    
+    
+  );
+}
+
+export default Navbar;
